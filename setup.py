2bbd7848e19bfd976fec3d95df175726ebacc837
DeanWu <pyli.xm@gmail.com>
2018-10-30 11:34:24 +0800

    修改版本号

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mdeditor',
-    version='0.1.8',
+    version='0.1.9',
     packages=find_packages(exclude=['mdeditor_demo', 'mdeditor_demo_app.*', 'mdeditor_demo_app']),
     include_package_data=True,
     license='GPL-3.0 License',
@@ -27,6 +27,7 @@ setup(
         'Framework :: Django :: 1.10',
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
