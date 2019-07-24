f975293f7e33679192f741c7c3d5943868b19420
Loner <yinjiakang@gmail.com>
2019-07-24 10:23:57 +0800

    use $math formula$ to display inline formula

--- mdeditor/static/mdeditor/js/editormd.js
+++ mdeditor/static/mdeditor/js/editormd.js
@@ -3596,7 +3596,7 @@
         };
 
         markedRenderer.paragraph = function(text) {
-            var isTeXInline     = /\$\$(.*)\$\$/g.test(text);
+            var isTeXInline     = /\$(.*)\$/g.test(text);
             var isTeXLine       = /^\$\$(.*)\$\$$/.test(text);
             var isTeXAddClass   = (isTeXLine)     ? " class=\"" + editormd.classNames.tex + "\"" : "";
             var isToC           = (settings.tocm) ? /^(\[TOC\]|\[TOCM\])$/.test(text) : /^\[TOC\]$/.test(text);
@@ -3604,7 +3604,7 @@
             
             if (!isTeXLine && isTeXInline) 
             {
-                text = text.replace(/(\$\$([^\$]*)\$\$)+/g, function($1, $2) {
+                text = text.replace(/(\$([^\$]*)\$)+/g, function($1, $2) {
                     return "<span class=\"" + editormd.classNames.tex + "\">" + $2.replace(/\$/g, "") + "</span>";
                 });
             } 
